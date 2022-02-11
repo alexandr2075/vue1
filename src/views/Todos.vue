53cf914e72d596d496e3788806e2ff5c6b76d69d
alex2075 <gagara5620@gmail.com>
2022-02-11 09:04:04 +0600

    add

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -3,34 +3,56 @@
     <h2>Todo application</h2>
     <router-link to="/">Home</router-link>
     <hr>
+    <Loader v-if="loading"/>
     <AddTodo
-    @add-todo="addTodo"
+        @add-todo="addTodo"
      />
+     <select v-model="filter">
+       <option value="all">All</option>
+       <option value="completed">Completed</option>
+       <option value="not-completed">Not Completed</option>
+     </select>
     <TodoList 
-    v-bind:todos="todos"
-    @remove-todo="removeTodo"
-    />  
+        v-eise-if="filteredTodos.length"
+        v-bind:todos="filteredTodos"
+        @remove-todo="removeTodo"
+    /> 
+    <p>No todos!</p> 
   </div>
 </template>
 
 <script>
 import TodoList from '@/components/TodoList'
 import AddTodo from '@/components/AddTodo'
+import Loader from '@/components/Loader'
 export default {
   name: 'App',
   data() {
     return {
-      todos: [
-      
-      ]
+      todos: [],
+      loading: true,
+      filter: 'all'
       }
   },
   mounted() {
     fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
       .then(response => response.json())
       .then(json => {
-        this.todos = json
+          this.todos = json
+          this.loading = false
       })
+  },
+  computed: {
+    filteredTodos() {
+      if(this.filter === 'all') {
+        return this.todos
+      }
+      if(this.filter === 'completed') {
+        return this.todos.filter(t => t.completed)
+      if(this.filter === 'not-completed') {
+        return this.todos.filter(t => !t.completed)
+      }
+    }
   },
   methods: {
     removeTodo(id) {
@@ -41,7 +63,7 @@ export default {
     }
   },
   components: {
-    TodoList, AddTodo
+    TodoList, AddTodo, Loader
   }
 }
 </script>
